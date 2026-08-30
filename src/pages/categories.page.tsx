78fadaf1d400af589a4d9aab71fa58803e83db8d
tutosrive <tutosrivegamer@gmail.com>
2026-08-30 10:30:01 -0500

    feat: Load categories with badges and nice 'random' styles

--- src/pages/categories.page.tsx
+++ src/pages/categories.page.tsx
@@ -1,41 +1,44 @@
 import { useEffect, useState } from 'react';
 import type { Topic } from '../app/models';
 import Loading from '../components/loading.component';
+import Helpers from '../app/utils/helpers.utils';
+import { Link } from 'react-router';
 
 const base = import.meta.env.VITE_API_URL;
 
 export default function CategoriesPage() {
-    const [categories, setCategories] = useState<Topic[]>();
+    const [categories, setCategories] = useState<Topic[]>([]);
     const fetchCategories = async () => {
         const req = await fetch(`${base}/topics`);
         const res: Topic[] = await req.json();
+
         const tmt = setTimeout(() => {
             if (req.ok === true && req.status === 200 && res instanceof Array) {
                 setCategories(res);
             }
             clearTimeout(tmt);
-        }, 3000);
+        }, 1000);
     };
 
     useEffect(() => {
         fetchCategories();
     }, []);
     return (
-        <div className="w-full h-full">
-            <div className="badge badge-info"></div>
-            {categories ? (
-                categories.map((cat) => {
-                    const typeB = ['primary', 'secondary', 'accent', 'neutral', 'info', 'success', 'warning', 'error'];
-                    const str = typeB[Math.floor(Math.random() * typeB.length)];
-                    return (
-                        <div key={cat.id} className={`badge badge-${str}`}>
-                            {cat.name}
-                        </div>
-                    );
-                })
-            ) : (
-                <Loading />
-            )}
+        <div className="w-full h-full flex flex-wrap items-center justify-center">
+            <div className="w-full flex flex-wrap items-center justify-center">
+                {categories.length > 0 ? (
+                    categories.map((cat) => {
+                        const badgeStyle: string = Helpers.getRandomBadgeStyle();
+                        return (
+                            <Link to={`/categories/${cat.name}`} key={cat.id} className={`m-1 badge-lg badge badge-soft ${badgeStyle}`}>
+                                {cat.name}
+                            </Link>
+                        );
+                    })
+                ) : (
+                    <Loading />
+                )}
+            </div>
         </div>
     );
 }
