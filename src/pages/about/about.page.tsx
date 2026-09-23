364b8d3b3519697607c4c09d1044a66fce637c4b
tutosrive <tutosrivegamer@gmail.com>
2026-09-23 13:34:07 -0500

    feat: Fully cache with @TanStack/query

--- src/pages/about/about.page.tsx
+++ src/pages/about/about.page.tsx
@@ -1,22 +1,15 @@
-import { useEffect, useState } from 'react';
 import Helpers from '../../app/utils/helpers.utils';
 import MarkdownPreview from '@uiw/react-markdown-preview';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
 
 export default function AboutPage() {
-    const [description, setDescription] = useState<string>();
     const tryGetPageReadme = async () => {
         const owner = 'tutosrive';
         const repo = 'openastral';
         const DEFAULT_URL = `https://raw.githubusercontent.com/${owner}/${repo}/main/README.md`;
-        const res = await Helpers.getReadme(owner, repo, DEFAULT_URL);
-        if (res && res.length > 0) {
-            setDescription(res);
-        }
+        return await Helpers.getReadme(owner, repo, DEFAULT_URL);
     };
-    useEffect(() => {
-        tryGetPageReadme();
-    }, []);
-
+    const { data } = useQuery<string | undefined>({ queryKey: ['about-page'], queryFn: tryGetPageReadme, notifyOnChangeProps: ['data'], placeholderData: keepPreviousData, staleTime: Infinity });
     return (
         <div className={'lg:mt-10 md:mt-10 mt-15 mb-5 p-5 transition-all w-[98dvw] lg:w-full md:w-full md-preview'}>
             <h1 className="text-center text-9xl text-primary britney-ft">
@@ -24,12 +17,12 @@ export default function AboutPage() {
                     Open Astral
                 </a>
             </h1>
-            {!description ? (
+            {!data ? (
                 <p>
                     <span className="italic text-neutral text-xs">No Description</span>
                 </p>
             ) : (
-                <MarkdownPreview source={description} className="z-0 w-full min-w-3" urlTransform={Helpers.goToCustom} />
+                <MarkdownPreview source={data} className="z-0 w-full min-w-3" urlTransform={Helpers.goToCustom} />
             )}
         </div>
     );
--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -6,43 +6,37 @@ import { PAGES_TITLES } from '../app/utils/constants';
 import SkeletonRepository from '../components/skeleton/repository.skeleton';
 import PaginationController from '../components/pagination.component';
 import CumulativeRepo from '../components/repository/cumulative.component';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
 
 export default function HomePage() {
-    const [repos, setRepos] = useState<Repository[]>([]);
     const [count, setCount] = useState<number>(0);
     const [currentPage, setCurrentPage] = useState<number>(() => {
         const session = sessionStorage.getItem('currentpage');
         return session ? parseInt(session) : 1;
     });
+    const { data } = useQuery<Repository[]>({ queryKey: ['repos', currentPage], queryFn: () => repositoryService.getPaginated(currentPage), notifyOnChangeProps: ['data'], placeholderData: keepPreviousData, staleTime: Infinity });
     const updateTitle = useWindowTitle((state) => state.updateTitle);
     const totalPages = Math.ceil(count / 20);
-
     const init = async () => {
         updateTitle(PAGES_TITLES.home);
         const resCount = await repositoryService.getDataCount();
         setCount(resCount);
-    };
-    const setReposs = async (page: number) => {
-        const res = await repositoryService.getPaginated(page);
-        setRepos(res);
-        setCurrentPage(page);
     };
     useEffect(() => {
         sessionStorage.setItem('currentpage', `${currentPage}`);
     }, [currentPage]);
     useEffect(() => {
         init();
-        setReposs(currentPage);
     }, []);
 
     return (
         <div id="home-page" className="w-dvw lg:px-5 md:px-5 sm:px-4 px-3 py-8">
-            {repos.length > 0 ? (
+            {data && data.length > 0 ? (
                 <div className="w-full h-full grid grid-cols-12 gap-1">
-                    {repos.map((repo) => {
+                    {data.map((repo) => {
                         return <CumulativeRepo key={`repo-${repo.id}`} repository={repo} />;
                     })}
-                    <PaginationController callback={setReposs} page={currentPage} totalPages={totalPages} />
+                    <PaginationController callback={setCurrentPage} page={currentPage} totalPages={totalPages} />
                 </div>
             ) : (
                 <SkeletonRepository />
