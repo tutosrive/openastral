ca75391a828bea37a61182b99b54e3681106aac6
tutosrive <tutosrivegamer@gmail.com>
2026-09-25 15:02:42 -0500

    fix: Handle null and zero elements when search

--- src/pages/search/search.page.tsx
+++ src/pages/search/search.page.tsx
@@ -13,19 +13,31 @@ export default function SearchPage() {
     const textToFind = searchParams.get('text') ?? '';
     const [countPages, setCountPages] = useState<number>(0);
     const [currentPage, setCurrentPage] = useState<number>(1);
+    const [hasError, setHasError] = useState<{ has: boolean; error: string | null | undefined | React.JSX.Element }>({ has: false, error: null });
     const type = useSearch((state) => state.type);
     const handleSearch = async () => {
         let result: Repository[] | null = null;
         if (type === 'mix' && textToFind) {
             const data = await repositoryService.getSearchMix(textToFind, currentPage);
-            if (data) {
-                result = data;
-            }
+            result = data;
         }
         return result;
     };
     const { data } = useQuery({ queryKey: ['search', textToFind, currentPage], queryFn: () => handleSearch(), notifyOnChangeProps: ['data'], placeholderData: keepPreviousData, staleTime: Infinity });
     const updateTotalPagesCalc = () => {
+        if (!data || data === null || Object.keys(data).length === 0 || data.length === 0) {
+            setHasError({
+                has: true,
+                error: (
+                    <span>
+                        No matching records found for <span className="text-error">{textToFind}</span>. Please try a different query.
+                    </span>
+                ),
+            });
+            return;
+        } else {
+            setHasError({ has: false, error: null });
+        }
         if (data) {
             const firstRepo = data[0] as Repository;
             if (firstRepo && firstRepo.totalcount) {
@@ -41,16 +53,20 @@ export default function SearchPage() {
         setCurrentPage(1);
     }, [textToFind]);
     return (
-        <div id="search-page" className="w-dvw px-5 py-8">
-            {data && data.length > 0 ? (
-                <div className="w-full h-full grid grid-cols-12 gap-1">
-                    {data.map((repo) => {
-                        return <CumulativeRepo repository={repo} key={`repo-${repo.id}`} />;
-                    })}
-                    <PaginationController callback={setCurrentPage} page={currentPage} totalPages={countPages} />
-                </div>
+        <div id="search-page" className="w-full h-full min-h-full px-5 py-8">
+            {hasError.has === false ? (
+                data && data.length > 0 ? (
+                    <div className="w-full h-full grid grid-cols-12 gap-1">
+                        {data.map((repo) => {
+                            return <CumulativeRepo repository={repo} key={`repo-${repo.id}`} />;
+                        })}
+                        <PaginationController callback={setCurrentPage} page={currentPage} totalPages={countPages} />
+                    </div>
+                ) : (
+                    <SkeletonRepository />
+                )
             ) : (
-                <SkeletonRepository />
+                <div className="w-full h-full flex items-center justify-center">{hasError.error}</div>
             )}
         </div>
     );
